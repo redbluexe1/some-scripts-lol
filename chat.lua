879dbdd47bfeeb454cd5e989f3d6e11ad44230af
redbluexe1 <86514546+redbluexe1@users.noreply.github.com>
2022-05-20 21:31:21 +0800

    chat.lua
    points at the model that u want to say

--- chat.lua
+++ chat.lua
@@ -1,5 +1,5 @@
 chats = {"first text", "second text", "third text"} -- what u want to make it say
 while true do 
 wait(5) --Time bethween chats
-game:GetService("Chat"):Chat(script.Parent.Head, chats[math.random(1, #chats)], Enum.ChatColor.Red)
+game:GetService("Chat"):Chat(script.Parent, chats[math.random(1, #chats)], Enum.ChatColor.Red)
 end
